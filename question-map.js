f0a0b4183b2b17443e11577c7c3195b0d267e8be
Kyle Jacobson <kylejacobson1987@gmail.com>
2019-12-22 12:40:05 -0800

    first commit

---
+++ question-map.js
@@ -0,0 +1,16 @@
+// Does the app fetch data?
+  // type of data (string)
+  // singular / list / usntructured
+
+// Does the app save data?
+  // singular / list
+  // can this data be deleted? 
+
+// What can the user interact with?
+  // buttons
+  // links
+  // forms
+    // text input
+    // select
+      // single select
+      // multiselect 
